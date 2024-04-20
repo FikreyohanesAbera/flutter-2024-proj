152a23a4ee67acfe6c00c0679395257bfbcad2e8
SamuelT1995 <133096205+SamuelT1995@users.noreply.github.com>
2024-04-20 14:51:22 +0300

    Admin page screen

---
+++ lib/Presentation/screens/admin_page.dart
@@ -0,0 +1,208 @@
+import 'package:flutter/material.dart';
+import 'package:admin/widgets/toggle_button.dart';
+import 'package:admin/widgets/date_picker.dart';
+
+class AdminPage extends StatelessWidget {
+  final bool isDarkMode;
+  final VoidCallback toggleTheme;
+
+  AdminPage({required this.isDarkMode, required this.toggleTheme});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Center(child: Text('Admin Page', style: TextStyle(color: Colors.white),)),
+        actions: [
+          ToggleButton(isDarkMode: isDarkMode, toggleTheme: toggleTheme),
+        ],
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Card(
+              child: Center(
+                child: DatePickerWidget(
+                  onDateSelected: (DateTime? pickedDate) {
+                    if (pickedDate != null) {
+                      // Do something with the selected date
+                      print('Selected date: $pickedDate');
+                    }
+                  },
+                ),
+              ),
+            ),
+            SizedBox(height: 16),
+            Card(
+              child: Padding(
+                padding: EdgeInsets.all(16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Add Table',
+                      style: Theme.of(context).textTheme.headline6,
+                      textAlign: TextAlign.center,
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(labelText: 'Total Number'),
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(labelText: 'Number of Seats'),
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(labelText: 'Floor Number'),
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(labelText: 'Type'),
+                    ),
+                    SizedBox(height: 16),
+                    Center(
+                      child: ElevatedButton(
+                        onPressed: () {
+                          // Implement add table functionality
+                        },
+                        child: Text('Add'),
+                        style: ElevatedButton.styleFrom(
+                          foregroundColor: Colors.white, backgroundColor: Colors.blueGrey[900],
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 16),
+            Card(
+              child: Center(
+                child: ListTile(
+                  title: Text('Get All Tables'),
+                  onTap: () {
+                    // Implement get all tables functionality
+                  },
+                ),
+              ),
+            ),
+            SizedBox(height: 16),
+            Card(
+              child: Padding(
+                padding: EdgeInsets.all(16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Get Single Table',
+                      style: Theme.of(context).textTheme.headline6,
+                      textAlign: TextAlign.center,
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(labelText: 'Table Number'),
+                    ),
+                    SizedBox(height: 16),
+                    Center(
+                      child: ElevatedButton(
+                        onPressed: () {
+                          // Implement get single table functionality
+                        },
+                        child: Text('Get Table'),
+                        style: ElevatedButton.styleFrom(
+                          foregroundColor: Colors.white, backgroundColor: Colors.blueGrey[900],
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 16),
+            Card(
+              child: Padding(
+                padding: EdgeInsets.all(16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Update Tables',
+                      style: Theme.of(context).textTheme.headline6,
+                      textAlign: TextAlign.center,
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(labelText: 'Total Number'),
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(labelText: 'Number of Seats'),
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(labelText: 'Floor Number'),
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(labelText: 'Type'),
+                    ),
+                    SizedBox(height: 16),
+                    Center(
+                      child: ElevatedButton(
+                        onPressed: () {
+                          // Implement update table functionality
+                        },
+                        child: Text('Update Table'),
+                        style: ElevatedButton.styleFrom(
+                          foregroundColor: Colors.white, backgroundColor: Colors.blueGrey[900],
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 16),
+            Card(
+              child: Padding(
+                padding: EdgeInsets.all(16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Delete Table',
+                      style: Theme.of(context).textTheme.headline6,
+                      textAlign: TextAlign.center,
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(labelText: 'Total Number of Tables to Delete'),
+                    ),
+                    SizedBox(height: 16),
+                    Center(
+                      child: ElevatedButton(
+                        onPressed: () {
+                          // Implement delete table functionality
+                        },
+                        child: Text('Delete Table'),
+                        style: ElevatedButton.styleFrom(
+                          foregroundColor: Colors.white, backgroundColor: Colors.blueGrey[900],
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
