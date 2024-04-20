ba2ba2189735c9ad30218f4fc92a7946c8a6c111
namesjohnnyabex <godfathershemsu@gmail>
2024-04-20 10:24:08 +0300

    modified bookingsClass

--- lib/Presentation/others/bookingsClass.dart
+++ lib/Presentation/others/bookingsClass.dart
@@ -5,7 +5,8 @@ class ReservedTable {
   String? type;
   String? imageUrl;
   String? food;
+  String? branch;
 
-  ReservedTable(
-      this.guests, this.date, this.time, this.type, this.imageUrl, this.food);
+  ReservedTable(this.guests, this.date, this.time, this.type, this.imageUrl,
+      this.food, this.branch);
 }
