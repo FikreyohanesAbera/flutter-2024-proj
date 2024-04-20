cc9691571191772b8a24ee638182fbe64b4df9fa
Zeamanuel-Admasu <149781558+Zeamanuel-Admasu@users.noreply.github.com>
2024-04-20 20:11:39 +0300

    Add files via upload

---
+++ lib/Presentation/screens/intro.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import '../widgets/intropage.dart';
+
+void main() {
+  runApp(const Intro());
+}
+
+class Intro extends StatelessWidget {
+  const Intro({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Intro Page',
+      home: Scaffold(
+          appBar: AppBar(
+            backgroundColor: const Color(0xff182032),
+            title: const Text(
+              'Hi there',
+              style: TextStyle(
+                fontFamily: 'A',
+                fontSize: 40,
+                color: Color.fromARGB(153, 56, 231, 202),
+              ),
+            ),
+          ),
+          body: const IntroBody()),
+    );
+  }
+}
