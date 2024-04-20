590802508bcc63f657255e75b372620e9b556570
Isam Ahmed <167436033+isamAhm@users.noreply.github.com>
2024-04-20 23:14:04 +0300

    final expanded_booking.dart

--- lib/Presentation/screens/expanded_booking.dart
+++ lib/Presentation/screens/expanded_booking.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import '../others/bookingsClass.dart';
+import '../others/booking_class.dart';
 import 'dart:async';
 import 'package:google_fonts/google_fonts.dart';
 
